0b5c53e44f0ffaf5ef03bc246a46a8ed9c7110bb
WPZ <1002386667@qq.com>
2022-05-10 11:10:10 +0800

    first commit

---
+++ src/main/java/admin/bean/City.java
@@ -0,0 +1,16 @@
+package admin.bean;
+
+import lombok.Data;
+
+/**
+ * @Author WPZ
+ * @Date 2022/5/6 10:36
+ * @Version 1.0
+ */
+@Data
+public class City {
+    private Long id;
+    private String name;
+    private String state;
+    private String country;
+}
